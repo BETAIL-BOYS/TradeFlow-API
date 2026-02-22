ab9e37bba8272b86531c4b5851a4330f3e4e0dd3
Oyeleke Amir <oyelekeamir123@gmail.com>
2026-02-22 16:15:08 +0100

    Setup swagger/openapi documentation

--- dist/app.service.d.ts
+++ dist/app.service.d.ts
@@ -1,5 +1,7 @@
+import { CreateInvoiceDto } from './invoices/dto/create-invoice.dto';
+import { InvoiceDto } from './invoices/dto/invoice.dto';
 export declare class AppService {
     private invoices;
-    getAllInvoices(): any[];
-    processNewInvoice(data: any): any;
+    getAllInvoices(): InvoiceDto[];
+    processNewInvoice(data: CreateInvoiceDto): InvoiceDto;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Vital for Frontend connection
+
+  const config = new DocumentBuilder()
+    .setTitle('TradeFlow API')
+    .setDescription('API documentation for the TradeFlow application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
