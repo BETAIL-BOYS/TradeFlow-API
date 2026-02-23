e4d790bda1015d7f70faf0165c8bbc6be3f75875
Your GitHub Username <your-email@example.com>
2026-02-23 01:36:43 +0100

    wallet-based authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { RiskModule } from './risk/risk.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HealthModule, RiskModule],
+  imports: [HealthModule, RiskModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
